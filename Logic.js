a0d5dc5758b3fcf9bf5ab26338aa7c695a4012f3
WaelKendouli <kendouliwael@gmail.com>
2026-08-27 22:18:23 +0200

    Adding the first UI object

--- Logic.js
+++ Logic.js
@@ -1,5 +1,12 @@
 
 const UI = {
- invalid : document.getElementById("")
+    txtCity: document.getElementById('txtCity'),
 
-}
+    btnSearch: document.getElementById('btn_search'),
+
+    countrySelect: document.getElementById('country_select'),
+
+    lbInvalid: document.getElementById('lbInvalid'),
+
+    tbTimes: document.getElementById('tbTimes')
+};
