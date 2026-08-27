0cec48e639e05065231ce153666db98ce9ee9c04
WaelKendouli <kendouliwael@gmail.com>
2026-08-27 23:06:14 +0200

    Added the function that builds the search URL with safty verification

--- Logic.js
+++ Logic.js
@@ -22,3 +22,27 @@ if (!message) {
 UI.lbInvalid.style.display = "flex";
 UI.lbInvalid.textContent = message;
 }
+
+function BulidURL(country , city , time) {
+
+    if(!country || !city || !time)
+    {
+        throw new Error("Missing required parameters");
+    }
+    if(typeof country !== "string" || typeof city !== "string" || typeof time !== "string" )
+    {
+            throw new Error("wrong type used in one of the parameters , all of them should be string");
+    }
+
+let parms = new URL(`timingsByCity/${time}`, Main_URL);
+parms.set("city", city);
+parms.set("country",country);
+
+return parms.toString();
+}
+
+
+async function SearchPrayerTime(country , city , time)
+{
+
+}
